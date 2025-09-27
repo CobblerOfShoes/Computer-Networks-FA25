0179a83c2f5bbf1b4a8cf9f5c79c72e33ecebc51
tparisi <timothy.parisi72@gmail.com>
2025-09-26 22:36:25 -0400

    Initial Server, swapping machines

---
+++ project1/server.cc
@@ -0,0 +1,114 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+
+
+#include "SocketHelper.h"
+
+using namespace std;
+// Create a function to run a GET command on a URL input, port 80?
+#define PORT "80"
+#define MAXDATASIZE 100
+#define URL "ns-mn1.cse.nd.edu/cse30264/ads/file1.html"
+
+int create_Out_Socket(char * address);
+
+int read_Website(char * url) {
+	
+    cout << url;
+    create_Out_Socket(url);
+	return 0;
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int create_Out_Socket(char * address) {
+    int sockfd, numbytes;  
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(address, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        inet_ntop(p->ai_family,
+            get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+        printf("client: attempting connection to %s\n", s);
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("client: connect");
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family,
+            get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connected to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    printf("client: received '%s'\n",buf);
+
+    close(sockfd);
+
+    return 0;
+
+}
+
+int main(int argc, char * argv[]) {
+	string buffer;
+    buffer = URL;
+    char * url = new char[BUFSIZ];
+    strncpy(url, buffer.c_str(), BUFSIZ-1);
+	read_Website(url);
+	return 0;
+
+}
