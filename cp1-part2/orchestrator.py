18ebfa4a7040922339724bd223fb7396a876c94f
tparisi <timothy.parisi72@gmail.com>
2025-10-12 10:41:28 -0400

    Multithreaded Orchestrator

--- cp1-part2/orchestrator.py
+++ cp1-part2/orchestrator.py
@@ -6,21 +6,63 @@ import time
 import math
 import logging
 import os
+from threading import Thread, Lock
+import queue
 
-MAX_WORKERS = 5
-WORKERS = []
+threads = []
+work_queue = queue.Queue()
+resp_queue = queue.Queue()
+lock = Lock()
 
-logging.basicConfig(
-    format="[{asctime}] {levelname} - {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M",
-)
 
-def alert_worker(message, worker):
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('port', type=int, help='The port number for the server')
+    parser.add_argument('workers', type=int, help='The number of workers to summon')
+    parser.add_argument('--verbose', help='Enable verbose output', action="store_true")
+
+    args = parser.parse_args()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if args.verbose:
+        print('Binding to port ' + str(args.port))
+
+    server_address = ('', args.port)
+    sock.bind(server_address)
+    if args.verbose: 
+        print('Success!')
+
+    t = Thread(target=resp_process)
+    threads.append(t)
+    t.start()
+
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            text = data.decode('utf-8')
+            if args.verbose: 
+                print(f"received message: {text}")
+            if 'REGISTER' in text:
+                t = Thread(target=worker_process, args=text[1:])
+                threads.append(t)
+                t.start()
+                if args.verbose: 
+                    print(text[1:])
+            if 'CHECK' in text:
+                work_queue.put((text, addr))
+        except Exception as e:
+            for t in threads:
+                t.close()
+            if args.verbose: 
+                print(f'Exception: {e}', file=sys.stderr)
+            sys.exit(1)
+
+def alert_worker(message, hostname, port):
     worker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f"Binding to port {worker['port']}")
-    worker['port'] = int(worker['port'])
-    worker_sock.connect((worker['hostname'], worker['port']))
+    print(f"Binding to port {port}")
+    port = int(port)
+    worker_sock.connect((hostname, port))
     print('Connected! Sending Message...')
     data = message.encode('utf-8')
     worker_sock.send(data)
@@ -33,63 +75,24 @@ def alert_worker(message, worker):
     
     print(response.decode('utf-8'))
     return response
-    
-def add_worker(addr, text):
-    if len(WORKERS) <= MAX_WORKERS:
-        text = text.split(' ')
-        try:
-            WORKERS.append({'hostname': text[1], 
-                            'port': text[2], 
-                            'identifier': text[3],
-                            'is_free': True})
-        except:
-            print("ERROR: Incorrect number of arguments")
-            return
-    else:
-        print("Error: More than {MAX_WORKERS} workers registered")
-        return
 
-def main():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('port', type=int, help='The port number for the server')
-    parser.add_argument('workers', type=int, help='The number of workers to summon')
-
-    args = parser.parse_args()
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print('Binding to port ' + str(args.port))
-
-    server_address = ('', args.port)
-    sock.bind(server_address)
-    print('Success!')
-
+def worker_process(hostname, port):
     while True:
-        data, addr = sock.recvfrom(1024)
-        text = data.decode('utf-8')
-        print(f"received message: {text}")
-        if 'REGISTER' in text:
-            add_worker(addr, text)
-            print(WORKERS)
+        try:
+            message = work_queue.get() 
+            response = alert_worker(message, hostname, port)
+            resp_queue.put(response)
+        finally:
+            work_queue.task_done()
 
-        if 'CHECK' in text:
-            worker_name = ''
-            for i, worker in enumerate(WORKERS):
-                if worker['is_free'] == True:
-                    worker['is_free'] = False
-                    worker_name = WORKERS[i]
-                    break
-            if not worker_name:
-                print("No workers free")
-                continue
-            pid = os.fork()
-            if pid == 0:
-                response = alert_worker(text, worker_name)
-                sock.sendto(response, (addr))
-                sys.exit(0)
-            elif pid < 0:
-                sys.exit(pid)
-                
-        
+def resp_process():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        try:
+            response, addr = resp_queue.get()
+            sock.sendto(response, (addr))
+        finally:
+            resp_queue.task_done()
 
 if __name__ == '__main__':
     main()
