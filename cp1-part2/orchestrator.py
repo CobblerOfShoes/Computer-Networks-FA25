150de77db8fc993b57f47a8e4338b0917681cfd8
tparisi <timothy.parisi72@gmail.com>
2025-10-12 16:45:38 -0400

    New Orchestrator, need to test on student machines

--- cp1-part2/orchestrator.py
+++ cp1-part2/orchestrator.py
@@ -2,42 +2,87 @@ import sys
 import socket
 import argparse
 import sys
+import time
+import math
 import logging
 import os
+from threading import Thread, Lock
+from datetime import datetime
+import queue
 import subprocess
 import signal
+import json
 
-from datetime import datetime
-
-MAX_WORKERS = 5
-workers: dict[str: dict] = {}
-
-# The following are globals such that the signal handler can do cleanup
-worker_processes: list[subprocess.Popen] = []
-udp_sock: socket.socket = None
-worker_sockets: list[socket.socket] = []
-
-logging.basicConfig(
-    format="[{asctime}] {levelname} - {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M",
-)
+threads = []
+work_queue = queue.Queue()
+resp_queue = queue.Queue()
+lock = Lock()
 
 def signal_handler(sig, frame):
-    for worker in worker_processes:
-        worker.terminate()
-    if (udp_sock is not None):
-        udp_sock.close()
-    for socket in worker_sockets:
-        if socket is not None:
-            socket.close()
+    for t in threads:
+        t.close()
+        sys.exit(sig)
+
+def main():
+    signal.signal(signal.SIGINT, signal_handler)
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('port', type=int, help='The port number for the server')
+    parser.add_argument('workers', type=int, help='The number of workers to summon')
+    parser.add_argument('--verbose', help='Enable verbose output', action="store_true")
+
+    args = parser.parse_args()
+
+    if args.port < 54000 or args.port > 54150:
+        print("ERROR: Please choose a port between 54000 and 54150")
+        sys.exit(1)
+
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if args.verbose:
+        print('Binding to port ' + str(args.port))
+
+    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ('', args.port)
+    udp_sock.bind(server_address)
+    if args.verbose:
+        print('Success!')
+
+    t = Thread(target=resp_process)
+    threads.append(t)
+    t.start()
+
     while True:
         try:
-            os.wait()
-        except ChildProcessError:
-            # No more child processes
-            break
-    sys.exit(0)
+            data, addr = udp_sock.recvfrom(1024)
+            text = data.decode('utf-8')
+            if args.verbose:
+                print(f"received message: {text}")
+            if 'REGISTER' in text:
+                t = Thread(target=worker_process, args=text[1:])
+                threads.append(t)
+                t.start()
+                if args.verbose:
+                    print(text[1:])
+
+                # Send back status info
+
+            if 'STATUS' in text:
+                send_status_info(addr, udp_sock)
+
+            # Send back last nth hits
+            if 'HITS' in text:
+                num_hits = text.split()[1]
+                send_last_hits(addr, udp_sock, num_hits)
+
+            if 'CHECK' in text:
+                work_queue.put((text, addr))
+        except Exception as e:
+            for t in threads:
+                t.close()
+            if args.verbose:
+                print(f'Exception: {e}', file=sys.stderr)
+            sys.exit(1)
 
 def update_latest_hits(worker_id, site_id, ad_string, time):
     lines = []
@@ -60,68 +105,6 @@ def update_latest_hits(worker_id, site_id, ad_string, time):
     with open('./worker_states/hits', 'w') as f:
         f.writelines(new_list)
 
-
-def alert_worker(message, worker, worker_id):
-    worker_sock = worker['socket']
-
-    worker['last_job_time'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    data = message.encode('utf-8')
-    worker_sock.send(data)
-
-    response = worker_sock.recv(2048)
-    if not response:
-        print("Socket Connection Broken")
-        sys.exit(1)
-    worker['last_response_time'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-
-    with open(f'./worker_states/{worker_id}', 'w') as f:
-        f.write(str({worker_id: worker}))
-
-    words = response.decode("utf-8").split()
-
-    update_latest_hits(worker_id, words[5], words[2], worker['last_response_time'])
-
-    response = " ".join(words[3:])
-
-    return response
-
-def add_worker(addr, text):
-    if len(workers) <= MAX_WORKERS:
-        text = text.split(' ')
-        try:
-          hostname = text[1]
-          port = text[2]
-          id = text[3]
-          is_free = True
-
-          worker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          worker_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-          print(f"Binding connection to {id} on port {port}")
-          worker_sock.connect((hostname, int(port)))
-
-          workers.update({id: {'hostname': hostname,
-                              'port': port,
-                              'is_free': is_free,
-                              'socket': worker_sock}})
-        except:
-            print("ERROR: Incorrect number of arguments")
-            return
-    else:
-        print("Error: More than {MAX_WORKERS} workers registered")
-        return
-
-def send_status_info(addr, udp_sock):
-    response = ""
-    for filename in os.listdir('./worker_states'):
-        file_path = os.path.join('./worker_states', filename)
-        with open(file_path, 'r', encoding="utf-8") as f:
-            response += f.read()
-
-    if not response.strip():
-        response = "No data yet"
-
-    udp_sock.sendto(response.encode("utf-8"), (addr))
-
 def send_last_hits(addr, udp_sock, n_hits):
     response = ""
     with open('./worker_states/hits', 'a+') as f:
@@ -141,87 +124,54 @@ def send_last_hits(addr, udp_sock, n_hits):
 
     udp_sock.sendto(response.encode("utf-8"), (addr))
 
-def main():
-    signal.signal(signal.SIGINT, signal_handler)
+def send_status_info(addr, udp_sock):
+    response = ""
+    for filename in os.listdir('./worker_states'):
+        file_path = os.path.join('./worker_states', filename)
+        with open(file_path, 'r', encoding="utf-8") as f:
+            response += f.read()
 
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('port', type=int, help='The port number for the server')
+    if not response.strip():
+        response = "No data yet"
 
-    args = parser.parse_args()
+    udp_sock.sendto(response.encode("utf-8"), (addr))
 
-    if args.port < 54000 or args.port > 54150:
-        print("ERROR: Please choose a port between 54000 and 54150")
+def alert_worker(message, hostname, port):
+    worker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    starttime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    print(f"Binding to port {port}")
+    port = int(port)
+    worker_sock.connect((hostname, port))
+    print('Connected! Sending Message...')
+    data = message.encode('utf-8')
+    worker_sock.send(data)
+    response = worker_sock.recv(2048)
+    endtime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    worker_sock.close()
+    if not response:
+        print("Socket Connection Broken")
         sys.exit(1)
 
-    #### Set up UDP listening socket
-    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print('Binding UDP listener to port ' + str(args.port))
+    print(response.decode('utf-8'))
+    return response
 
-    # Tell the OS we know what we are doing
-    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    host_ip = socket.gethostname()
-
-    server_address = ('127.0.0.1', args.port)
-    udp_sock.bind(server_address)
-
-    tasks: dict = {}
-
+def worker_process(hostname, port):
     while True:
-        data, addr = udp_sock.recvfrom(1024)
-        text = data.decode('utf-8')
-        print(f"received message: {text}")
+        try:
+            message = work_queue.get()
+            response = alert_worker(message, hostname, port)
+            resp_queue.put(response)
+        finally:
+            work_queue.task_done()
 
-        # Worker is contacting Orchestrator for the first time
-        if 'REGISTER' in text:
-            add_worker(addr, text)
-
-        # Send back status info
-        if 'STATUS' in text:
-            send_status_info(addr, udp_sock)
-
-        # Send back last nth hits
-        if 'HITS' in text:
-            num_hits = text.split()[1]
-            send_last_hits(addr, udp_sock, num_hits)
-
-        if 'PING' in text:
-            response = "PONG\r\n\r\n".encode("utf-8")
+def resp_process():
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        try:
+            response, addr = resp_queue.get()
             udp_sock.sendto(response, (addr))
-
-        # A client has submitted a request
-        if 'CHECK' in text:
-            available_worker = None
-            worker_id = None
-            for worker_name, worker in workers.items():
-                if worker['is_free'] == True:
-                    worker['is_free'] = False
-                    available_worker = worker
-                    worker_id = worker_name
-                    break
-
-            if not available_worker:
-                # Wait for an available worker
-                child_pid, status = os.waitpid(-1, 0)
-                worker_id, available_worker = tasks[child_pid]
-                available_worker['is_free'] = True
-                del tasks[child_pid]
-
-            pid = os.fork()
-            if pid == 0:
-                # We are the child
-                response = alert_worker(text, available_worker, worker_id)
-                udp_sock.sendto(response.encode("utf-8"), (addr))
-                sys.exit(0)
-            elif pid < 0:
-                # Disaster
-                sys.exit(pid)
-            else:
-                # We are the parent
-                # Track which worker was assigned to which client
-                tasks.update({pid: (worker_id, available_worker)})
-
-
+        finally:
+            resp_queue.task_done()
 
 if __name__ == '__main__':
     main()
