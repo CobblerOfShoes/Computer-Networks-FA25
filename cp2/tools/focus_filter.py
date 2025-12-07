e80dfba3e4bf886b06fe875f59fa885331bae1e8
Alexander Schumann <aschuma4@student13.cse.nd.edu>
2025-12-07 06:17:43 -0500

    Reworking filtering to use a generator expression to improve speed, and reworking pto to use pcapwriter to improve speed

--- cp2/tools/focus_filter.py
+++ cp2/tools/focus_filter.py
@@ -5,7 +5,7 @@ import sys
 import find_client
 import os
 
-def main(filename, file_out):
+def filter_tcp(filename):
     if not os.path.isfile(filename):
         print(f"ERROR: Could not find file {filename}")
         sys.exit(1)
@@ -16,16 +16,19 @@ def main(filename, file_out):
     print(f"Server IP Address: {server_ip}")
     print("Filtering out all extra packets...")
 
-    # Filter out packets not between client and server
     for packet in scapy.PcapReader(filename):
         if packet.haslayer(IP):
             if (packet[IP].src == client_ip and packet[IP].dst == server_ip) or (packet[IP].src == server_ip and packet[IP].dst == client_ip):
-                scapy.wrpcap(file_out, packet, append=True)
+                yield packet
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Find client IP addresses from pcap files.")
     parser.add_argument("pcap_file", help="Path to the pcap file to analyze.")
     parser.add_argument("file_out", help="Path to the output pcap file.")
     args = parser.parse_args()
 
-    main(args.pcap_file, args.file_out)
+    with scapy.PcapWriter(args.file_out, append=True, sync=True) as wr:
+        wr.write(filter_tcp(args.pcap_file, args.file_out))
+
+if __name__ == "__main__":
+    main()
