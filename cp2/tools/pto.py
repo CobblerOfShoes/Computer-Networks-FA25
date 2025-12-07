d7126778422940cbddb5d8ca24ff6ee21137f3e9
Alexander Schumann <aschuma4@student13.cse.nd.edu>
2025-12-07 04:30:03 -0500

    Updating other scripts to use scapy.PcapReader instead of rdpcap as well

--- cp2/tools/pto.py
+++ cp2/tools/pto.py
@@ -2,20 +2,30 @@ import argparse
 from find_tcp_max import header_length
 import scapy.all as scapy
 
+import os
+import sys
+
 def trim_packets(filename):
-    packets = scapy.rdpcap(filename)
-    
-    for packet in packets:
-        pkt_len = header_length(packet)
-        new_packet = bytes(packet)[:pkt_len]
-        new_filename = filename.split('/')
-        new_filename = '/'.join(new_filename[:-1]) + '/pto-' + new_filename[-1]
-        scapy.wrpcap(f"{new_filename}", new_packet, append=True)
-        
+    if not os.path.isfile(filename):
+        print(f"ERROR: Could not find file {filename}")
+        sys.exit(1)
+
+    for packet in scapy.PcapReader(filename):
+        # Prune TCP Only (trim payload)
+        if scapy.TCP in packet:
+          pkt_len = header_length(packet)
+          new_packet = bytes(packet)[:pkt_len]
+          new_filename = filename.split('/')
+          new_filename = '/'.join(new_filename[:-1]) + '/pto-' + new_filename[-1]
+          scapy.wrpcap(f"{new_filename}", new_packet, append=True)
+        # Leave other packet types alone
+        else:
+          scapy.wrpcap(f"{new_filename}", packet, append=True)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find the maximum TCP header length in a pcap file.")
     parser.add_argument("pcap_file", help="Path to the pcap file to analyze.")
     args = parser.parse_args()
-    
+
     trim_packets(args.pcap_file)
